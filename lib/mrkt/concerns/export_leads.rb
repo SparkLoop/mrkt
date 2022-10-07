7869b44840f9671c78a5a53a3ae7dbcca9257291
Petar <risteskipetar3@gmail.com>
2022-10-07 16:45:47 +0200

    change method names

--- lib/mrkt/concerns/export_leads.rb
+++ lib/mrkt/concerns/export_leads.rb
@@ -1,6 +1,6 @@
 module Mrkt
   module ExportLeads
-    def create_job(fields: [], column_header_names: {}, start_at: nil, end_at: nil)
+    def create_export_job(fields: [], column_header_names: {}, start_at: nil, end_at: nil)
       post_json('/bulk/v1/leads/export/create.json') do
         params = {
           fields: fields,
@@ -18,19 +18,19 @@ module Mrkt
       end
     end
 
-    def start_job(id)
+    def start_export_job(id)
       post("/bulk/v1/leads/export/#{id}/enqueue.json")
     end
 
-    def check_job_status(id)
+    def check_export_job_status(id)
       get("/bulk/v1/leads/export/#{id}/status.json")
     end
 
-    def retrieve_job_data(id)
+    def retrieve_export_job_data(id)
       get("/bulk/v1/leads/export/#{id}/file.json")
     end
 
-    def cancel_job(id)
+    def cancel_export_job(id)
       post("/bulk/v1/leads/export/#{id}/cancel.json")
     end
   end
